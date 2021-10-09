cb01d0d7cbc07af29d7a132c9b22fa9417b69820
Jordan_Torres <jt.arontorres@gmail.com>
2021-10-08 20:21:08 -0700

    adding user connected and disconnected

--- index.js
+++ index.js
@@ -9,25 +9,23 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-// io.on('connection', (socket) => {
-//     console.log('a user connected');
-//     socket.on('disconnect', () => {
-//       console.log('user disconnected');
-//     });
-//   });
-
-// io.on('connection', (socket) => {
-//     socket.on('chat message', (msg) => {
-//         console.log('message: ' + msg);
-//     });
-// });
-
 io.on('connection', (socket) => {
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+    console.log('a user connected');
+    io.emit('user connected');
+
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+        io.emit('user disconnected');
     });
-  });
-  
+
+    socket.on('chat message', (msg) => {
+        socket.broadcast.emit('chat message', msg);
+    });
+
+    
+});
+
 server.listen(3000, () => {
   console.log('listening on *:3000');
 });
